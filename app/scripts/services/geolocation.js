7073d5693c1731da7a7c72d666b84fcf691c100b
MMiszy <lupinek7@gmail.com>
2014-01-07 15:54:37 +0100

    LB-3 Added fetching data from 4sq

---
+++ app/scripts/services/geolocation.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('lunchButtonApp')
+  .service('Geolocation', ['$q', function Geolocation($q) {
+    this.getCurrentPosition = function () {
+      var deferred = $q.defer();
+
+      navigator.geolocation.getCurrentPosition(function (pos) {
+        deferred.resolve(pos);
+      });
+
+      return deferred.promise;
+    };
+  }]);
---
+++ test/spec/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('App setup', function () {
+  beforeEach(module('lunchButtonApp'));
+
+  it('should have 4sq config constants', inject(function ($injector) {
+    var sq = $injector.get('FOURSQUARE');
+    expect(sq).toBeTruthy();
+  }));
+});
